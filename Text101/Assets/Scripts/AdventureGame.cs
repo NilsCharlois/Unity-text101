3c5c4c2c1ca42f56f0d04006f77aea4ccba27547
Nils Charlois <nils.charlois@gmail.com>
2019-10-28 18:37:05 +0000

    Lecture 30 finished

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
+		
+	State state;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		SetCurrentState(startingState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+	
+	public void ManageState()
+	{
+		var nextStates = state.GetNextStates();
+		if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+		{
+			SetCurrentState(nextStates[0]);			
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+		{
+			SetCurrentState(nextStates[1]);						
+		}
+	}
+	public void SetCurrentState(State stateToSet)
+	{
+		this.state = stateToSet;
+        textComponent.text = state.GetStateStory();
+	}
+}
